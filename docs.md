bc3f8e0abd71fa0b606d2087742f6821ca475693
JB Kam <jb3.kam@gmail.com>
2023-04-03 20:05:09 -0700

    basic setup done

--- docs.md
+++ docs.md
@@ -6,3 +6,12 @@ Installations:
     - both express and mongoose (to interact with mongo)
 3. npm install --save-dev dotenv nodemon
     - create env
+
+
+
+Project Setup:
+1. create .env
+2. create script in package.json
+    -  "devStart": "nodemon server.js"
+3. create server.js file
+4. start express server
---
+++ routes/subscribers.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const router = express.Router()
+
+//Getting all
+router.get('/', (req, res) => {
+
+
+
+})
+//Getting one
+router.get('/:id', (req, res) => {
+
+
+
+})
+// Creating One
+router.post('/', (req, res) => {
+
+
+
+})
+// Updating One
+router.patch('/', (req, res) => {
+
+
+
+})
+// Deleting One
+router.delete('/:id', (req, res) => {
+
+
+
+})
+
+module.exports = router
